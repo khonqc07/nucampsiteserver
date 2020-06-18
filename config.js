7ed35775abe379a5898640f658a2d7347bfd0fb1
khonqc07 <khonqc07@gmail.com>
2020-06-18 16:06:11 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '469862500519713',
+        clientSecret: '314ace4a7babbe5904bbc3147e925928'
+    }
 }

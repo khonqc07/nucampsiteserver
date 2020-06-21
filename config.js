10c386bbd7d5b82e64b838999086b60e0d73936a
khonqc07 <60451280+khonqc07@users.noreply.github.com>
2020-06-21 15:45:40 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '469862500519713',
-        clientSecret: '314ace4a7babbe5904bbc3147e925928'
-    }
-}
